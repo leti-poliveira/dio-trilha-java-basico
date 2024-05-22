2fcc3bf859aa7b6aa570cf4beceeb6387af2f57d
leticia.paula <leticiapaulade@gmail.com>
2024-05-21 23:49:23 -0300

    atualizações de aula.

---
+++ aulas-de-java/src/edu/leticia/terminaleargumentos/MinhaClasse.java
@@ -0,0 +1,13 @@
+package edu.leticia.terminaleargumentos;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+    System.out.println("Oi Turma!!! Fui executado pelo Terminal");
+        }
+    }
+
+    
+
+
+
