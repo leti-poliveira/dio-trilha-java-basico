b434c9f1ad507d2eaeef32f6e786858b012e703b
leticia.paula <leticiapaulade@gmail.com>
2024-05-22 23:44:23 -0300

    Inclusão da aula: controle de fluxo.

---
+++ aulas-de-java/src/edu/leticia/controledefluxo/CondicionalSimples.java
@@ -0,0 +1,16 @@
+public class CondicionalSimples {
+
+public static void main(String[] args)  {
+
+// CaixaEletronico.java
+
+       double saldo = 25.0;
+       double valorSolicitado = 17.0;
+
+       if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
+}
+}
