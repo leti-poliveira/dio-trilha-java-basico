84ecdb90a26f9b917799a892a026b73dc40a56e0
leticia.paula <leticiapaulade@gmail.com>
2024-05-26 11:00:11 -0300

    atualização de aulas

---
+++ aulas-de-java/src/edu/leticia/controledefluxo/ExemploFor.java
@@ -0,0 +1,14 @@
+public class ExemploFor {
+    
+// ExemploFor.java
+
+	public static void main(String[] args) {
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+	}
+}
+
+
+
+
